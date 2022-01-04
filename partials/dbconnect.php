c8c3685a3b0f90bc489c801ea1d318aa8f4a81a0
Aman <amanullatamboli5@gmail.com>
2022-01-04 11:12:44 +0530

    4 jan

--- partials/dbconnect.php
+++ partials/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
   $hostname = "localhost";
   $username = "root";
-  $password = "komal7781";
+  $password = "";
   $dbname = "temp";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
